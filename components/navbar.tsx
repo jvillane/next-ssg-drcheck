665e101ae3b91e4d1bc1544c6b7542238e42053c
jvillane <jvillane@gmail.com>
2021-04-04 16:36:43 -0400

    FIX: links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import { CheckCircleOutline } from "heroicons-react";
 import { Transition } from "@headlessui/react";
 import React, { useRef, useState } from "react";
 import { useClickAway } from "react-use";
+import Link from "next/link";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,12 +21,14 @@ export default function Navbar() {
              aria-label="Global">
           <div className="flex items-center flex-1">
             <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="/">
-                <span className="sr-only">DrCheck</span>
-                <div className="text-white">
-                  <CheckCircleOutline/>
-                </div>
-              </a>
+              <Link href="/">
+                <a href="#">
+                  <span className="sr-only">DrCheck</span>
+                  <div className="text-white">
+                    <CheckCircleOutline/>
+                  </div>
+                </a>
+              </Link>
               <div className="-mr-2 flex items-center md:hidden">
                 <button type="button"
                         className="bg-primary rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
@@ -40,9 +43,15 @@ export default function Navbar() {
               </div>
             </div>
             <div className="hidden space-x-8 md:flex md:ml-10">
-              <a href="/" className="text-base font-medium text-white hover:text-gray-300">Inicio</a>
-              <a href="/servicios" className="text-base font-medium text-white hover:text-gray-300">Servicios</a>
-              <a href="/equipo" className="text-base font-medium text-white hover:text-gray-300">Equipo</a>
+              <Link href="/">
+                <a href="#" className="text-base font-medium text-white hover:text-gray-300">Inicio</a>
+              </Link>
+              <Link href="/servicios">
+                <a href="#" className="text-base font-medium text-white hover:text-gray-300">Servicios</a>
+              </Link>
+              <Link href="/equipo">
+                <a href="#" className="text-base font-medium text-white hover:text-gray-300">Equipo</a>
+              </Link>
             </div>
           </div>
           <div className="hidden md:flex md:items-center md:space-x-6">
@@ -101,15 +110,18 @@ export default function Navbar() {
             </div>
             <div className="pt-5 pb-6">
               <div className="px-2 space-y-1">
-                <a href="#"
-                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Inicio</a>
-                
-                <a href="#"
-                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Servicios</a>
-                
-                <a href="#"
-                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Equipo</a>
-              
+                <Link href="/">
+                  <a href="#"
+                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Inicio</a>
+                </Link>
+                <Link href="/servicios">
+                  <a href="#"
+                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Servicios</a>
+                </Link>
+                <Link href="/equipo">
+                  <a href="#"
+                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">Equipo</a>
+                </Link>
               </div>
               <div className="mt-6 px-5">
                 <a href="mailto:consulta@drcheck.cl"
